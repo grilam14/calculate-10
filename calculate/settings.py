54ba859c4c7c04ff6649687e0f8d2dcc8e7c3af8
grilam14 <andrew.grillot@gmail.com>
2020-06-10 18:58:02 -0500

    udpate settings

--- calculate/settings.py
+++ calculate/settings.py
@@ -52,6 +52,10 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'calculate.urls'
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
